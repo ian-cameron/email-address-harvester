05be0bcc2185f50a15367136c5eb88177c045eb6
ian-cameron <ian@digiwest.com>
2023-11-20 12:21:26 -0800

    Add breadth-first crawling algorithm

--- src/emailharvester.py
+++ src/emailharvester.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import queue
 import sys
 import csv
 import time
@@ -11,8 +12,17 @@ import os
 if not len(sys.argv) == 2:
     print(f'Missing required argument.  Provide a URL.\nUsage example: python {sys.argv[0]} https://site-to-crawl.com/contact/')
     exit(2)
-base_url = sys.argv[1]
+base_url = sys.argv[1].lower()
+
+# Set crawler user agent string
 request_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+# Set max requests per second
+crawl_rate = 5
+# Set max depth of crawl
+max_depth = 10
+# Set common extentions to ignore that likely aren't text/html
+excluded =['doc','docx','docm','ppt','ppsx','pptx','xls','xlsx','xlsm','accdb','png','jpg','jpeg','mov','mp4','wmv','gif','bmp','dgn','pdf','eps','ai','zip','dll','exe','rss','csv','txt']
+
 
 # Verify the URL
 try: response = requests.get(base_url, headers=request_headers)
@@ -30,9 +40,6 @@ results_file = f'{filename}-emails.csv'
 
 visited_urls = set()
 email_set = set()
-
-# Don't visit urls that end with common file extensions, you'd have to download them and they very likely aren't text/html
-excluded =['doc','docx','docm','ppt','ppsx','pptx','xls','xlsx','xlsm','accdb','png','jpg','jpeg','mov','mp4','wmv','gif','bmp','dgn','pdf','eps','ai','zip','dll','exe','rss','csv','txt']
 
 # Initialize results file if it doesn't exist
 if not os.path.isfile(results_file):
@@ -58,8 +65,13 @@ with open(history_file, mode='r', newline='', encoding='utf-8') as file:
     for row in reader:
         visited_urls.add(row['URL'].lower())
 
-# Recursive function definition
-def scrape_email_addresses(url):
+# Breadth-first recursive site crawling function definition
+def scrape_email_addresses(queue):
+    if queue.empty():
+        return
+    item = queue.get()
+    url = item[0]
+    depth = item[1]
     try: response = requests.get(url, headers=request_headers)
     except Exception as e:
         print(f'REQUEST ERROR: {url}\n{getattr(e, 'message', repr(e))}')
@@ -72,7 +84,8 @@ def scrape_email_addresses(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     total_links = len(soup.find_all('a', href=True))
     skipped_links = prior_links = new_links = email_links = prior_emails = 0
-    for link in soup.find_all('a', href=True):
+    # Find non-js and non-anchor links
+    for link in soup.find_all("a", href=re.compile(r"^(?!javascript|#|tel|data|ftp)")):
         # Save mailto: links and continue
         if link.has_attr('href') and link['href'].startswith('mailto:'):
             email = link['href'][7:].split('?')[0].strip()            
@@ -80,33 +93,73 @@ def scrape_email_addresses(url):
                 email_links += 1
                 text = link.text.strip()
                 found_email(email, text, find_context(link, soup.title.text), url)
+                continue
             else:
                 prior_emails += 1
                 continue
-        # Ignore URL fragments
-        next_url = link.get('href').split('#')[0]
-        # Skip external sites and known file types
-        if next_url.split('.')[-1].split('?')[0].split('#')[0].lower() in excluded or not next_url.startswith('/') and not next_url.startswith(base_url):
-            skipped_links += 1
-            continue
-        next_url = re.sub(r'(?<!:)//','/',base_url + next_url.replace(base_url,'/'))
+        # Crawl non-mailto: links
+        next_url = normalize_url(link.get('href'), url)
         # Skip visited URLs
         if next_url.lower() in visited_urls:
             prior_links += 1
             continue
+        # Don't go deeper than max_depth
+        if depth < 0:
+            print(f'Crawling depth limit reached, skipping {next_url} from {url}')
+            skipped_links += 1
+            continue
+        # Skip external sites and known file types
+        if skip_url(next_url):
+            skipped_links += 1
+            continue
+        # Rate limit crawler
+        time.sleep(1/crawl_rate)
         visited_url(next_url)
-        time.sleep(0.2)
-        print(f'Now crawling {next_url}') 
+        queue.put((next_url, depth-1))
         new_links += 1    
-        scrape_email_addresses(next_url)
+        
     # Check for any remaining emails not in a mailto:
-    emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', soup.get_text())
+    emails = reduce_generator(soup.stripped_strings)
     for email in emails:
         if email.lower() not in email_set:
             found_email(email, email, soup.title.text, url)
 
     # Print a summary after all links from a page have been checked
-    print(f'Completed crawling {url.replace(base_url,'')}:\n\t{total_links}\ttotal links.\n\t{new_links}\tnewly discovered.\n\t{prior_links}\talready crawled.\n\t{skipped_links}\texcluded.\n\t{email_links}\tnew emails found.\n\t{prior_emails}\texisting emails skipped.')          
+    print(f'Completed crawling {url.replace(base_url,'')} ({abs(-max_depth + depth)} levels from root):\n \
+          \t{total_links}\ttotal links.\n \
+          \t{new_links}\tnewly discovered.\n \
+          \t{prior_links}\talready crawled.\n \
+          \t{skipped_links}\texcluded.\n \
+          \t{email_links}\tnew emails found.\n \
+          \t{prior_emails}\texisting emails skipped.')
+    
+    scrape_email_addresses(queue)
+
+def normalize_url(url, referer):
+    # Remove fragments
+    url = url.split('#')[0].lower()
+    # Remove extra directory slashes
+    url = re.sub(r'(?<!:)//+','/', url)
+    # Add base URL host to site relative URLs
+    if url.startswith('/'):
+        return re.match(r"https?://[^/]+",base_url).group(0) + url
+    # Add referer to page relative URL without querystring params
+    if not url.startswith('http'):
+        return re.sub(r"(?<!/)$", "/", referer.split('?')[0]) + url
+    return url  
+
+# Determine if a URL should not be crawled
+def skip_url(url):
+     # Skip urls that are probably files eg /docs/service_terms.pdf?Page=1
+     if url.split('.')[-1].split('?')[0].split('#')[0].lower() in excluded:
+         return True
+     # Consider optional www in URLs to be same base URL
+     if not "://www." in base_url:
+        url = url.replace("://www.", "://")
+     # Skip external urls
+     if not url.startswith(base_url):
+        return True
+     return False
 
 # Write visited URL to log file and save in the global list
 def visited_url(url):
@@ -130,9 +183,10 @@ def found_email(email, text, context, url):
 # Find some text closest to the link
 def find_context(link, fallback): 
     try:
+        text = clean_txt(link.text)
         # Any adjacent text
         context = clean_txt(link.next_sibling.text) if link.next_sibling else ''
-        if context == '':
+        if context in ['', text]:
             context = clean_txt(link.previous_sibling.text) if link.previous_sibling else ''
         parents = link.find_parents() if context == '' else []
         if context == '':
@@ -165,6 +219,17 @@ def parent_search(parents, depth, i = None):
     text = clean_txt(parents[i].find(re.compile('^h[1-6]$')).text) if parents[i].find(re.compile('^h[1-6]$')) else ''
     return parent_search(parents, depth, i+1)
 
+# Find all emails contained in text snippets
+def reduce_generator(generator):
+    hits = [re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',text) \
+            for text in generator \
+            if re.search(r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}', text)]
+    unique_elements = set()
+    for array in hits:
+        for element in array:
+            unique_elements.add(element)
+    return list(unique_elements)
+
 # Display difference between start and end timestamps
 def print_time_delta(start_ts, end_ts):
     ts_diff = end_ts - start_ts
@@ -183,8 +248,10 @@ print(f'Starting with {starting_urls} URLs and {starting_emails} emails.')
 visited_url(base_url)
 
 # Enter the main program loop
-scrape_email_addresses(base_url)
-
+queue = queue.Queue()
+queue.put((base_url,max_depth))
+scrape_email_addresses(queue)
+                       
 # Record ending performance statistics
 ending = datetime.now()
 total_emails = sum(1 for line in open(results_file, encoding='utf-8'))-1
